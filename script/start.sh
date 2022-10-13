2be87b2f65d65ac573feb683532e499b5bc3695f
woootae <92504193+woootae@users.noreply.github.com>
2022-10-13 17:19:53 +0900

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -10,7 +10,7 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 echo "$TIME_NOW : copy $JAR_FILE" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+cp $PROJECT_ROOT/Sample/target/*.jar $JAR_FILE
 
 echo "$TIME_NOW : run $JAR_FILE" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
